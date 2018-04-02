a30b99714adf1a4060524d4213e9108c0e5c2d59
devsoochan <devsoochan@gmail.com>
2018-04-02 11:29:50 +0900

    초기 커밋.

---
+++ RecoWeb.Domain/Abstract/IMesEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecoWeb.Domain.Concrete;
+using System.Data.Entity.Core.Objects;
+
+namespace RecoWeb.Domain.Abstract
+{
+    public interface IMesEntityRepository
+    {
+        IEnumerable<COW_MenuListByJsonInquiry_Result> COW_MenuListByJsonInquiry();
+
+        int PRW_EmployeeSave(string id, string password, string name, string email, string phonenumber, ObjectParameter outmessage);
+
+        IEnumerable<COW_DataStackInquiry_Result> COW_DataStackInquiry(string code);
+
+        void COW_AuthenticateInquiry(string id, string password, ObjectParameter outResult, ObjectParameter outMessage);
+
+        IEnumerable<PRW_PlantInquiry_Result> PRW_PlantInquiry();
+
+        IEnumerable<PRW_EmployeeInquiry_Result> PRW_EmployeeInquiry(string category);
+    }
+}
---
+++ RecoWeb.Domain/Concrete/MesEntityRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecoWeb.Domain.Abstract;
+using System.Data.Entity.Core.Objects;
+
+namespace RecoWeb.Domain.Concrete
+{
+    public class MesEntityRepository : IMesEntityRepository
+    {
+        private RecowebdbEntities context = new RecowebdbEntities();     
+
+        public IEnumerable<COW_MenuListByJsonInquiry_Result> COW_MenuListByJsonInquiry(){
+            return context.COW_MenuListByJsonInquiry(); 
+        }
+        
+
+        public int PRW_EmployeeSave(string id,string password, string name, string email, string phonenumber, ObjectParameter outmessage)
+        {
+            return context.PRW_EmployeeSave(id, password, name, email, phonenumber, outmessage);
+        }
+
+        public IEnumerable<COW_DataStackInquiry_Result> COW_DataStackInquiry(string code)
+        {
+            return context.COW_DataStackInquiry(code);
+        }
+
+        public void COW_AuthenticateInquiry(string id, string password, ObjectParameter outResult, ObjectParameter outMessage)
+        {
+            context.COW_AuthenticateInquiry(id, password, outResult, outMessage);
+        }
+
+        public IEnumerable<PRW_PlantInquiry_Result> PRW_PlantInquiry()
+        {
+            return context.PRW_PlantInquiry();
+        }
+
+        public IEnumerable<PRW_EmployeeInquiry_Result> PRW_EmployeeInquiry(string category)
+        {
+            return context.PRW_EmployeeInquiry(category);
+        }
+    }
+}
